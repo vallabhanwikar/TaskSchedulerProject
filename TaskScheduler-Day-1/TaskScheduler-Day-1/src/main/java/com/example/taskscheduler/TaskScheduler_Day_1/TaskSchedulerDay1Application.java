0d5ca36b86ecd6e36a8af64c3fbabd47d7ddb8f4
Vallabh Anwikar <vallabh.anwikar@gmail.com>
2025-08-11 22:16:56 +0530

    Initial commit

---
+++ TaskScheduler-Day-1/TaskScheduler-Day-1/src/main/java/com/example/taskscheduler/TaskScheduler_Day_1/TaskSchedulerDay1Application.java
@@ -0,0 +1,13 @@
+package com.example.taskscheduler.TaskScheduler_Day_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerDay1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskSchedulerDay1Application.class, args);
+	}
+
+}
---
+++ TaskScheduler-Day-1/TaskScheduler-Day-1/src/test/java/com/example/taskscheduler/TaskScheduler_Day_1/TaskSchedulerDay1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskscheduler.TaskScheduler_Day_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerDay1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
