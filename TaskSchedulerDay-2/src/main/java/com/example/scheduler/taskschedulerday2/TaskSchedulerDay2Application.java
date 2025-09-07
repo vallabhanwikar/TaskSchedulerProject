5d8198d052da6ffbed45d7899035cd73c4b35400
Vallabh Ramesh Anwikar <vallabh.anwikar@gmail.com>
2025-09-07 22:26:43 +0530

    Day-2 Started

---
+++ TaskSchedulerDay-2/src/main/java/com/example/scheduler/taskschedulerday2/TaskSchedulerDay2Application.java
@@ -0,0 +1,13 @@
+package com.example.scheduler.taskschedulerday2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskSchedulerDay2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskSchedulerDay2Application.class, args);
+    }
+
+}
---
+++ TaskSchedulerDay-2/src/test/java/com/example/scheduler/taskschedulerday2/TaskSchedulerDay2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.scheduler.taskschedulerday2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskSchedulerDay2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
